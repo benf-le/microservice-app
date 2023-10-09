a9e64997fb3e26c2c3ed8a22e4d7c8c8127fa690
benf-le <banglebang2016@gmail.com>
2023-10-10 04:49:49 +0700

    delete

--- employeeservice/src/main/java/com/benfle/employeeservice/command/data/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.benfle.employeeservice.command.data;
-
-import com.benfle.bookservice.command.data.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
-}
-
-//tu day tao ra ca truy van
