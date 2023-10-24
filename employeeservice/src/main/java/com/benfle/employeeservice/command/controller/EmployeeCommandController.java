960892dcf327e3aec49f8212f23f2c3e1e76cc01
benf-le <banglebang2016@gmail.com>
2023-10-25 01:29:57 +0700

    udpate

--- employeeservice/src/main/java/com/benfle/employeeservice/command/controller/EmployeeCommandController.java
+++ employeeservice/src/main/java/com/benfle/employeeservice/command/controller/EmployeeCommandController.java
@@ -10,14 +10,14 @@ import com.benfle.employeeservice.command.model.EmployeeRequestModel;
 
 
 import org.springframework.cloud.stream.annotation.EnableBinding;
-<<<<<<< Updated upstream
+
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.cloud.stream.messaging.Source;
 import com.fasterxml.jackson.core.JsonProcessingException;
-=======
+
 
 import org.springframework.cloud.stream.messaging.Source;
->>>>>>> Stashed changes
+
 import org.springframework.messaging.MessageChannel;
 import org.springframework.web.bind.annotation.*;
 import org.axonframework.commandhandling.gateway.CommandGateway;
@@ -32,10 +32,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 @RestController
 @RequestMapping("/api/v1/employees")
 @EnableBinding(Source.class)
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
+
+
 public class EmployeeCommandController {
 
     @Autowired
@@ -44,10 +43,7 @@ public class EmployeeCommandController {
     @Autowired
     private MessageChannel output;
 
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 
     @PostMapping
     public String addEmployee(@RequestBody EmployeeRequestModel model) {
@@ -76,10 +72,7 @@ public class EmployeeCommandController {
     @PostMapping("/sendMessage")
     public void SendMessage(@RequestBody String message) {
         try {
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
             ObjectMapper mapper = new ObjectMapper();
             String json = mapper.writeValueAsString(message);
             output.send(MessageBuilder.withPayload(json).build());
@@ -88,8 +81,5 @@ public class EmployeeCommandController {
             e.printStackTrace();
         }
     }
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 }
