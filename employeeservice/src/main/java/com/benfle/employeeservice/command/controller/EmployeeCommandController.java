76e77b2d57079fd4eee47b9b617f0517d5b2a949
benf-le <banglebang2016@gmail.com>
2023-10-25 01:35:40 +0700

    update

--- employeeservice/src/main/java/com/benfle/employeeservice/command/controller/EmployeeCommandController.java
+++ employeeservice/src/main/java/com/benfle/employeeservice/command/controller/EmployeeCommandController.java
@@ -77,7 +77,7 @@ public class EmployeeCommandController {
             String json = mapper.writeValueAsString(message);
             output.send(MessageBuilder.withPayload(json).build());
         } catch (JsonProcessingException e) {
-
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
